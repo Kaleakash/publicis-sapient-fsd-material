9109dce72c51759fcad0fe0853324bb4c1d0fe6b
Kishor <kishor1124@gmail.com>
2022-06-23 00:13:14 +0530

    Activity on User

--- Examples/Spring/spring-boot-demo.zip_expanded/spring-boot-demo/src/main/java/com/example/controller/MyRestApi.java
+++ Examples/Spring/spring-boot-demo.zip_expanded/spring-boot-demo/src/main/java/com/example/controller/MyRestApi.java
@@ -1,13 +1,19 @@
 package com.example.controller;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.beans.User;
@@ -23,13 +29,29 @@ public class MyRestApi {
 	
 	// storing the user
 	@PostMapping
-	public User store(@RequestBody User user) {
+	public ResponseEntity<Object> store(@RequestBody User user) {
 		User createdUser = service.storeUser(user);
-		return createdUser;
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
 	}
 	// fetching all the users
 	@GetMapping
-	public List<User> fetchUsers() {
-		return service.getAllUsers();
+	public ResponseEntity<Object> fetchUsers() {
+		List<User> users = service.getAllUsers();
+		return ResponseEntity.status(HttpStatus.OK).body(users);
+	}
+	
+	@GetMapping("/abc/{userId}")
+	public ResponseEntity<Object> getMessage(@PathVariable("userId") int id, 
+		 @RequestParam("username") String name) {
+		Map<String, String> map = new HashMap<String, String>();
+		if(id < 0) {
+			map.put("error", "id is invalid: "+id);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
+		} else {
+			map.put("message", "Valid id: "+id);
+			map.put("name", name);
+			return ResponseEntity.status(HttpStatus.OK).body(map);
+		}
+		
 	}
 }
