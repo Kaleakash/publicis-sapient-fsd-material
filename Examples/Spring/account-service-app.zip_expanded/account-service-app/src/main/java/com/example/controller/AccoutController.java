876175b176cd371174930e480aadd317f8ac7357
Kishor <kishor1124@gmail.com>
2022-06-27 21:00:00 +0530

    knowing about the multiple instances

--- Examples/Spring/account-service-app.zip_expanded/account-service-app/src/main/java/com/example/controller/AccoutController.java
+++ Examples/Spring/account-service-app.zip_expanded/account-service-app/src/main/java/com/example/controller/AccoutController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccoutController {
 
+	@Value("${server.port}") // this reads the port number of your application
+	private String port;
+	
 	@GetMapping
 	public String greet() {
+		System.out.println("Request arrived at "+port+"!");
 		return "Hello! this is Account Microservice";
 	}
 }
